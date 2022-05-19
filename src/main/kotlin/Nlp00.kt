50cc9aecc69c49ad5c20ff17373267e208737d40
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-05-19 23:25:07 +0900

    done 00

--- src/main/kotlin/Nlp00.kt
+++ src/main/kotlin/Nlp00.kt
@@ -1,3 +1,7 @@
+/**
+ * 00. Reversed stringPermalink
+ * Obtain the string that arranges letters of the string “stressed” in reverse order (tail to head).
+ */
 fun main(){
-    println("hello")
+    println("stressed".reversed())
 }
