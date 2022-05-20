68c73d9f03790c6cb09c4e09cce8819493250272
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-05-21 01:01:32 +0900

    done 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
     application
 }
 
@@ -14,6 +15,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1") // http client
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
 
 tasks.test {
---
+++ src/main/kotlin/Nlp20.kt
@@ -0,0 +1,31 @@
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.zip.GZIPInputStream
+
+/**
+ * 20. Read JSON documents
+ * Read the JSON documents and output the body of the article about the United Kingdom. Reuse the output in problems 21-29.
+ */
+const val JSON_URL = "https://nlp100.github.io/data/enwiki-country.json.gz"
+fun main() {
+    val wikiPages = fetchEnWikiCountries()
+    println(wikiPages["United Kingdom"])
+}
+
+fun fetchEnWikiCountries(): Map<String, String> {
+    val response = JSON_URL.httpGet().response().second
+    val unzipped = GZIPInputStream(response.data.inputStream())
+        .bufferedReader(UTF_8)
+        .use { it.readText() }
+    return unzipped.split("\n")
+        .filter { it != "" }
+        .map {
+            val obj = Json.decodeFromString<WikiPage>(it)
+            obj.title to obj.text
+        }.toMap()
+}
+
+@kotlinx.serialization.Serializable
+data class WikiPage(val title: String, val text: String)
